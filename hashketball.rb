572b465830ce84568e8838e49fceb8a20904e02e
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-05 03:10:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,21 @@ def game_hash
   { player_name: "Alan Anderson",
     number: "0",
     shoe: 16,
-    points: 22
-    rebounds: 12
-    assists: 12
-    steals: 3
-    blocks: 1
+    points: 22,
+    rebounds: 12,
+    assists: 12,
+    steals: 3,
+    blocks: 1,
     slam_dunks: 1
+  }, { player_name: "",
+    number: "",
+    shoe: ,
+    points: ,
+    rebounds: ,
+    assists: ,
+    steals: ,
+    blocks: ,
+    slam_dunks: 
   }, 
   
   
