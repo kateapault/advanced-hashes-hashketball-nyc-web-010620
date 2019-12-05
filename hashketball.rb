9a334c07f03217180899754d16dcb02ab3712fbd
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-05 05:22:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -221,15 +221,15 @@ def most_points_scored
 end
 
 def get_team_points(player_points_hash)
-  player_points_array.reduce(0) {|memo, playerpoints| memo += playerpoints}
+  player_points_array.reduce(0) {|memo, playerpoints| memo += playerpoints[1]}
 end
 
 def winning_team
   players_and_points_scored = get_player_and_stat(:points)
   teams = team_names
   players_and_points_scored = players_and_points_scored.to_a
-  team1 = players_and_points_scored[0...5].to_h
-  team2 = players_and_points_scored[5...10].to_h
+  team1 = players_and_points_scored[0...5]
+  team2 = players_and_points_scored[5...10]
   team1_points = get_team_points(team1)
   team2_points = get_team_points(team2)
   
