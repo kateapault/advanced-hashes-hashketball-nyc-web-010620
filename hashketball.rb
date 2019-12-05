71ae09548e955855ef932743ced3953f2dd952a6
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-05 04:54:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,9 +191,9 @@ def player_stats(player)
     team[1][:players]
   end
   p players
-#  selected_player_info = players.find do |players_on_team| 
-#    players_on_team.map {|guy| guy[:player_name] == player}
-#  end
+  selected_player_info = players.find do |players_on_team| 
+    players_on_team.map {|guy| p guy}
+  end
   
 #  p selected_player_info[0].delete(:player_name)
 #  p selected_player_info[0]
